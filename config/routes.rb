2988ec7c91f90c486cdbb3a65f1fa67d23d21bde
mc962 <11969409+mc962@users.noreply.github.com>
2022-02-04 18:46:31 -0500

    Add user authentication. Configure action storage. Add custom error handling. Configure mailer.
    Signed-off-by: mc962 <11969409+mc962@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: 'landings#index'
 
+  devise_for :users
+
+  resources :posts
+
   # Static Pages
   # NOTE: Each route is mapped manually to pages#show. The specific page/view to render is handled by adding an id param
   #   to the route, which the controller then resolves to the appropriate view.
@@ -10,4 +14,10 @@ Rails.application.routes.draw do
   get '/roadmap', to: 'pages#show', id: 'roadmap', as: :roadmap
   #noinspection RailsParamDefResolve
   get '/attributions', to: 'pages#show', id: 'attributions', as: :attributions
+
+  # Routes for Error Handling
+  match '/403', to: 'errors#forbidden', via: :all
+  match '/404', to: 'errors#not_found', via: :all
+  match '/422', to: 'errors#unprocessable_entity', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
 end
