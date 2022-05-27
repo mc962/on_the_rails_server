d8d2a99c8c8728f1223c64f1c9775abb6b8984c2
mc962 <11969409+mc962@users.noreply.github.com>
2022-05-27 16:36:35 -0400

    Style flashes
    Signed-off-by: mc962 <11969409+mc962@users.noreply.github.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  ALLOWED_FLASHES = %w[primary secondary success danger warning info light dark]
+
+  def bs_flash(alert_type)
+    if ALLOWED_FLASHES.include?(alert_type.to_s)
+      alert_type
+    else
+      'primary'
+    end
+  end
 end
