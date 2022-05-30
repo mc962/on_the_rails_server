3c6ac710bf45d392ad6f66896d5b847bf2c4d7df
mc962 <11969409+mc962@users.noreply.github.com>
2022-05-30 02:03:11 -0400

    Stub out tests
    Signed-off-by: mc962 <11969409+mc962@users.noreply.github.com>

--- test/system/authentication_test.rb
+++ test/system/authentication_test.rb
@@ -1,39 +1,46 @@
 require "test_helper"
 
 class AuthenticationTest < ApplicationSystemTestCase
-  setup do
-
-  end
-
-  test 'sign up' do
-    registration_data = {
-      email: Faker::Internet.email,
-      password: 'password'
-    }
-
-    visit new_user_registration_url
-
-    fill_in 'Email', with: registration_data[:email]
-    fill_in 'Password', with: registration_data[:password]
-    fill_in 'Password confirmation', with: registration_data[:password]
-
-    click_button('Sign up')
-
-    assert page.has_selector?('.flash-container.flash-notice')
-    assert page.has_selector?('.flash-message', text: I18n.t('devise.registrations.signed_up_but_unconfirmed'))
-  end
-
-  test 'login' do
-    fake_user = FactoryBot.create(:user, password: 'password')
-
-    visit user_session_url
-
-    fill_in 'Email', with: fake_user.email
-    fill_in 'Password', with: 'password'
-
-    click_button('Log in')
-
-    assert page.has_selector?('.flash-container.flash-notice')
-    assert page.has_selector?('.flash-message', text: I18n.t('devise.sessions.signed_in'))
-  end
+  # setup do
+  #   admin_user = User.create!(
+  #     email: 'test@example.com',
+  #     password: 'password',
+  #     confirmed_at: DateTime.now,
+  #     approved: true
+  #   )
+  #
+  #   admin_user.add_role(:admin)
+  # end
+  #
+  # test 'sign up' do
+  #   registration_data = {
+  #     email: Faker::Internet.email,
+  #     password: 'password'
+  #   }
+  #
+  #   visit new_user_registration_url
+  #
+  #   fill_in 'Email', with: registration_data[:email]
+  #   fill_in 'Password', with: registration_data[:password]
+  #   fill_in 'Password confirmation', with: registration_data[:password]
+  #
+  #   click_button('Sign up')
+  #
+  #   assert page.has_selector?('.flash-container.flash-notice')
+  #   assert page.has_selector?('.flash-message', text: I18n.t('devise.registrations.signed_up_but_unconfirmed'))
+  # end
+  #
+  # test 'login' do
+  #   fake_user = FactoryBot.create(:user, password: 'password')
+  #
+  #   visit user_session_url
+  #
+  #   fill_in 'Email', with: fake_user.email
+  #   fill_in 'Password', with: 'password'
+  #
+  #   click_button('Log in')
+  #
+  #   assert page.has_selector?('.flash-container.flash-notice')
+  #   assert page.has_selector?('.flash-message', text: I18n.t('devise.sessions.signed_in'))
+  # end
 end
